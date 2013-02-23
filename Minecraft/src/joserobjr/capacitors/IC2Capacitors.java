3d980052fcc39a68a9492dd283c8a2663d9777d2
jose.rob.jr <jose.rob.jr@gamemods.com.br>
2013-02-23 14:07:52 -0300

    Ambiente criado

---
+++ Minecraft/src/joserobjr/capacitors/IC2Capacitors.java
@@ -0,0 +1,9 @@
+package joserobjr.capacitors;
+
+import cpw.mods.fml.common.DummyModContainer;
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid="IC2Capacitors", version="1.0-MC1.4.7")
+public class IC2Capacitors extends DummyModContainer {
+	
+}
